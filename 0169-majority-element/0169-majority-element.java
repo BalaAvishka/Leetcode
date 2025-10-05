b3e62cef0545cda6a7fe81103460a965fb78ae7b
Bala Avishka A <balaavishka4@gmail.com>
2025-10-05 08:44:30 +0530

    Time: 2 ms (74.15%), Space: 53.4 MB (30.67%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int majority=nums[0],j=1;
+        for(int i=1;i<nums.length;i++)
+        {
+            if(j==0){
+            j++;
+            majority=nums[i];
+        }
+            else if(majority==nums[i])
+            {
+                j++;
+            }else
+            j--;
+    }
+    return majority;
+}}
